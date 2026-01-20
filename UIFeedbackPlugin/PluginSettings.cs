418febe835f6aff029c905dcdf508139c2039b6c
ハイさん <saisaisaiyoshiyoshiyoshi@gmail.com>
2026-01-20 19:05:39 +0900

    レイアウトなどを変更

--- UIFeedbackPlugin/PluginSettings.cs
+++ UIFeedbackPlugin/PluginSettings.cs
@@ -18,7 +18,6 @@ namespace InputSoundPlugin
         private bool _isInitialized = false;
         private long _lastKeyTime = 0;
         private long _lastMouseTime = 0;
-
         private readonly HashSet<Key> _pressedKeys = new();
         private readonly HashSet<MouseButton> _pressedMouseButtons = new();
 
@@ -29,6 +28,7 @@ namespace InputSoundPlugin
 
         public ObservableCollection<KeySoundSetting> KeySettings { get; set; } = new();
         public ObservableCollection<MouseSoundSetting> MouseSettings { get; set; } = new();
+        public List<string>? CopyBuffer { get; set; }
 
         private bool _isEnabled = true;
         public bool IsEnabled { get => _isEnabled; set => this.Set(ref _isEnabled, value, nameof(IsEnabled)); }
@@ -44,11 +44,7 @@ namespace InputSoundPlugin
 
         public override void Initialize()
         {
-            if (KeySettings.Count == 0 && MouseSettings.Count == 0)
-            {
-                ResetAllSettings(false);
-            }
-
+            if (KeySettings.Count == 0 && MouseSettings.Count == 0) ResetAllSettings(false);
             if (_isInitialized) return;
             EventManager.RegisterClassHandler(typeof(UIElement), UIElement.PreviewKeyDownEvent, new KeyEventHandler(OnKeyDown), true);
             EventManager.RegisterClassHandler(typeof(UIElement), UIElement.PreviewKeyUpEvent, new KeyEventHandler(OnKeyUp), true);
@@ -64,37 +60,37 @@ namespace InputSoundPlugin
                 var res = MessageBox.Show("すべての設定を初期化しますか？", "設定リセット", MessageBoxButton.YesNo, MessageBoxImage.Warning);
                 if (res != MessageBoxResult.Yes) return;
             }
-
             KeySettings.Clear();
-            var keys = Enum.GetValues(typeof(Key)).Cast<Key>().Where(k => k != Key.None).Distinct().OrderBy(k => k.ToString());
+            var keys = Enum.GetValues(typeof(Key)).Cast<Key>().Where(k => k != Key.None && k != Key.ImeProcessed).Distinct().OrderBy(k => k.ToString());
             foreach (var key in keys) KeySettings.Add(new KeySoundSetting { Key = key });
-
+            AddShiftSymbol(Key.D1, "!"); AddShiftSymbol(Key.D2, "\""); AddShiftSymbol(Key.D3, "#"); AddShiftSymbol(Key.D4, "$");
+            AddShiftSymbol(Key.D5, "%"); AddShiftSymbol(Key.D6, "&"); AddShiftSymbol(Key.D7, "'"); AddShiftSymbol(Key.D8, "(");
+            AddShiftSymbol(Key.D9, ")"); AddShiftSymbol(Key.OemMinus, "="); AddShiftSymbol(Key.OemPlus, "+"); AddShiftSymbol(Key.Oem5, "|");
             MouseSettings.Clear();
             foreach (MouseButton btn in Enum.GetValues(typeof(MouseButton))) MouseSettings.Add(new MouseSoundSetting { Button = btn });
         }
 
+        private void AddShiftSymbol(Key key, string symbol) => KeySettings.Add(new KeySoundSetting { Key = key, Modifiers = ModifierKeys.Shift, CustomName = $"Shift + {key} ({symbol})" });
+
         private void OnKeyDown(object sender, KeyEventArgs e)
         {
             if (!IsEnabled || e.IsRepeat) return;
-            var key = e.Key == Key.System ? e.SystemKey : e.Key;
+            var key = e.Key == Key.ImeProcessed ? e.ImeProcessedKey : (e.Key == Key.System ? e.SystemKey : e.Key);
             if (_pressedKeys.Contains(key)) return;
-
             long now = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
             if (now - _lastKeyTime < 2) return;
             _lastKeyTime = now;
-
             _pressedKeys.Add(key);
-            ProcessSound(KeySettings.FirstOrDefault(x => x.Key == key), true);
+            ProcessSound(KeySettings.FirstOrDefault(x => x.Key == key && x.Modifiers == Keyboard.Modifiers), true);
         }
 
         private void OnKeyUp(object sender, KeyEventArgs e)
         {
             if (!IsEnabled) return;
-            var key = e.Key == Key.System ? e.SystemKey : e.Key;
+            var key = e.Key == Key.ImeProcessed ? e.ImeProcessedKey : (e.Key == Key.System ? e.SystemKey : e.Key);
             if (!_pressedKeys.Contains(key)) return;
-
             _pressedKeys.Remove(key);
-            ProcessSound(KeySettings.FirstOrDefault(x => x.Key == key), false);
+            ProcessSound(KeySettings.FirstOrDefault(x => x.Key == key && x.Modifiers == Keyboard.Modifiers), false);
         }
 
         private void OnMouseDown(object sender, MouseButtonEventArgs e)
@@ -102,11 +98,9 @@ namespace InputSoundPlugin
             if (!IsEnabled) return;
             var btn = e.ChangedButton;
             if (_pressedMouseButtons.Contains(btn)) return;
-
             long now = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
             if (now - _lastMouseTime < 2) return;
             _lastMouseTime = now;
-
             _pressedMouseButtons.Add(btn);
             ProcessSound(MouseSettings.FirstOrDefault(x => x.Button == btn), true);
         }
@@ -116,7 +110,6 @@ namespace InputSoundPlugin
             if (!IsEnabled) return;
             var btn = e.ChangedButton;
             if (!_pressedMouseButtons.Contains(btn)) return;
-
             _pressedMouseButtons.Remove(btn);
             ProcessSound(MouseSettings.FirstOrDefault(x => x.Button == btn), false);
         }
@@ -128,60 +121,30 @@ namespace InputSoundPlugin
             else if (!isDown && setting.OnRelease) PlayRandom(setting.Paths);
         }
 
-        public void PlayRandom(ObservableCollection<string> paths)
+        public void PlayDirect(string path)
         {
-            var path = paths[_random.Next(paths.Count)];
             if (Directory.Exists(path))
             {
-                var files = Directory.EnumerateFiles(path, "*.*")
-                    .Where(f => {
-                        var ext = Path.GetExtension(f).ToLower();
-                        return ext == ".wav" || ext == ".mp3" || ext == ".mp4" || ext == ".avi" || ext == ".wmv";
-                    }).ToArray();
-                if (files.Length > 0) path = files[_random.Next(files.Length)];
-                else return;
+                var files = Directory.EnumerateFiles(path, "*.*").Where(f => {
+                    var ext = Path.GetExtension(f).ToLower();
+                    return ext == ".wav" || ext == ".mp3" || ext == ".mp4" || ext == ".avi" || ext == ".wmv";
+                }).ToArray();
+                if (files.Length > 0) path = files[_random.Next(files.Length)]; else return;
             }
             if (!File.Exists(path)) return;
-
-            var fullPath = Path.GetFullPath(path);
-            var currentVolume = Volume / 100.0;
-            var durationLimit = MaxDuration;
-
-            Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Send, new Action(() =>
-            {
-                if (MaxConcurrentSounds > 0 && _activePlayers.Count >= MaxConcurrentSounds)
-                {
-                    var oldPlayer = _activePlayers[0];
-                    oldPlayer.Stop();
-                    oldPlayer.Close();
-                    _activePlayers.RemoveAt(0);
-                }
-
+            Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Send, new Action(() => {
                 var player = new MediaPlayer();
-                player.Open(new Uri(fullPath));
-                player.Volume = currentVolume;
-                _activePlayers.Add(player);
-
-                if (durationLimit > 0)
-                {
-                    var timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(durationLimit) };
-                    timer.Tick += (s, e) => { player.Stop(); CleanupPlayer(player); timer.Stop(); };
-                    timer.Start();
-                }
-
-                player.MediaEnded += (s, e) => CleanupPlayer(player);
+                player.Open(new Uri(Path.GetFullPath(path)));
+                player.Volume = Volume / 100.0;
+                player.MediaEnded += (s, e) => player.Close();
                 player.Play();
             }));
         }
 
-        private void CleanupPlayer(MediaPlayer player)
-        {
-            player.Close();
-            _activePlayers.Remove(player);
-        }
+        public void PlayRandom(ObservableCollection<string> paths) { if (paths.Count > 0) PlayDirect(paths[_random.Next(paths.Count)]); }
     }
 
-    public interface ISoundSetting { bool OnPress { get; set; } bool OnRelease { get; set; } ObservableCollection<string> Paths { get; } }
-    public class KeySoundSetting : ISoundSetting { public Key Key { get; set; } public string Name => Key.ToString(); public bool OnPress { get; set; } = true; public bool OnRelease { get; set; } = false; public ObservableCollection<string> Paths { get; set; } = new(); }
+    public interface ISoundSetting { string Name { get; } bool OnPress { get; set; } bool OnRelease { get; set; } ObservableCollection<string> Paths { get; } }
+    public class KeySoundSetting : ISoundSetting { public Key Key { get; set; } public ModifierKeys Modifiers { get; set; } = ModifierKeys.None; public string? CustomName { get; set; } public string Name => CustomName ?? Key.ToString(); public bool OnPress { get; set; } = true; public bool OnRelease { get; set; } = false; public ObservableCollection<string> Paths { get; set; } = new(); }
     public class MouseSoundSetting : ISoundSetting { public MouseButton Button { get; set; } public string Name => Button.ToString(); public bool OnPress { get; set; } = true; public bool OnRelease { get; set; } = false; public ObservableCollection<string> Paths { get; set; } = new(); }
 }
--- UIFeedbackPlugin/SettingView.xaml.cs
+++ UIFeedbackPlugin/SettingView.xaml.cs
@@ -4,93 +4,246 @@ using System.Windows.Data;
 using System.Windows.Input;
 using Microsoft.Win32;
 using System.IO;
+using System.Windows.Media;
+using System.Text.Json;
+using System.Collections;
+using System.Globalization;
+using System.Windows.Markup;
 
 namespace InputSoundPlugin
 {
+    public class PathToFileNameConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => (value is string path) ? Path.GetFileName(path) : "";
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+        public override object ProvideValue(IServiceProvider serviceProvider) => this;
+    }
+
     public partial class SettingView : UserControl
     {
         private readonly PluginSettings _settings;
         private bool _isWaitingInput = false;
+        private bool _isUpdatingUI = false;
 
         public SettingView(PluginSettings settings)
         {
             InitializeComponent();
             _settings = settings;
             DataContext = settings;
-            CollectionViewSource.GetDefaultView(_settings.KeySettings).Filter = obj => FilterFunc((obj as KeySoundSetting)?.Name);
-            CollectionViewSource.GetDefaultView(_settings.MouseSettings).Filter = obj => FilterFunc((obj as MouseSoundSetting)?.Name);
+            CollectionViewSource.GetDefaultView(_settings.KeySettings).Filter = obj => FilterItem(obj as ISoundSetting);
+            CollectionViewSource.GetDefaultView(_settings.MouseSettings).Filter = obj => FilterItem(obj as ISoundSetting);
         }
-        private bool FilterFunc(string? name) => string.IsNullOrWhiteSpace(SearchBox.Text) || (name?.IndexOf(SearchBox.Text, StringComparison.OrdinalIgnoreCase) >= 0);
-        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e) { CollectionViewSource.GetDefaultView(_settings.KeySettings).Refresh(); CollectionViewSource.GetDefaultView(_settings.MouseSettings).Refresh(); }
+
+        private bool FilterItem(ISoundSetting? setting)
+        {
+            if (setting == null) return false;
+            if (!string.IsNullOrWhiteSpace(SearchBox.Text) && setting.Name.IndexOf(SearchBox.Text, StringComparison.OrdinalIgnoreCase) < 0) return false;
+            bool hasConfig = setting.Paths.Count > 0;
+            if (FilterConfigured.IsChecked == true && FilterNotConfigured.IsChecked == false && !hasConfig) return false;
+            if (FilterNotConfigured.IsChecked == true && FilterConfigured.IsChecked == false && hasConfig) return false;
+            if (setting is KeySoundSetting kss)
+            {
+                bool anyCat = FilterAZ.IsChecked == true || Filter09.IsChecked == true || FilterF.IsChecked == true || FilterCtrl.IsChecked == true || FilterOther.IsChecked == true;
+                if (!anyCat) return true;
+                var k = kss.Key;
+                if (FilterAZ.IsChecked == true && k >= Key.A && k <= Key.Z && kss.Modifiers == ModifierKeys.None) return true;
+                if (Filter09.IsChecked == true && (((k >= Key.D0 && k <= Key.D9) || (k >= Key.NumPad0 && k <= Key.NumPad9)) || kss.Modifiers == ModifierKeys.Shift)) return true;
+                if (FilterF.IsChecked == true && k >= Key.F1 && k <= Key.F24) return true;
+                string n = kss.Name.ToLower();
+                if (FilterCtrl.IsChecked == true && (n.Contains("ctrl") || n.Contains("alt") || n.Contains("shift") || n.Contains("win"))) return true;
+                if (FilterOther.IsChecked == true)
+                {
+                    bool isAZ = k >= Key.A && k <= Key.Z;
+                    bool is09 = (k >= Key.D0 && k <= Key.D9) || (k >= Key.NumPad0 && k <= Key.NumPad9);
+                    bool isF = k >= Key.F1 && k <= Key.F24;
+                    bool isCtrl = n.Contains("ctrl") || n.Contains("alt") || n.Contains("shift") || n.Contains("win");
+                    if (!isAZ && !is09 && !isF && !isCtrl) return true;
+                }
+                return false;
+            }
+            return true;
+        }
+
+        private void Filter_Changed(object sender, RoutedEventArgs e) => RefreshFilters();
+        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e) => RefreshFilters();
+        private void RefreshFilters() { CollectionViewSource.GetDefaultView(_settings.KeySettings).Refresh(); CollectionViewSource.GetDefaultView(_settings.MouseSettings).Refresh(); }
+
+        private void FilterButton_Click(object sender, RoutedEventArgs e) => FilterPopup.IsOpen = FilterButton.IsChecked == true;
+        private void CloseFilter_Click(object sender, RoutedEventArgs e) { FilterPopup.IsOpen = false; FilterButton.IsChecked = false; }
+
+        private void Selection_Changed(object sender, SelectionChangedEventArgs e)
+        {
+            if (_isUpdatingUI) return;
+            var list = sender as ListBox;
+            if (list == null) return;
+
+            bool isKey = list == KeyListBox;
+            var area = isKey ? KeyDetailArea : MouseDetailArea;
+            var title = isKey ? KeyDetailTitle : MouseDetailTitle;
+            var pathList = isKey ? KeyPathList : MousePathList;
+            var press = isKey ? KeyOnPressCheck : MouseOnPressCheck;
+            var release = isKey ? KeyOnReleaseCheck : MouseOnReleaseCheck;
+
+            _isUpdatingUI = true;
+            if (list.SelectedItems.Count > 0)
+            {
+                area.Visibility = Visibility.Visible;
+                var selectedItems = list.SelectedItems.Cast<ISoundSetting>().ToList();
+                var firstItem = selectedItems[0];
+                title.Text = selectedItems.Count == 1 ? firstItem.Name : $"{firstItem.Name} (他{selectedItems.Count - 1}件選択中)";
+                pathList.ItemsSource = firstItem.Paths;
+                press.IsChecked = selectedItems.All(x => x.OnPress);
+                release.IsChecked = selectedItems.All(x => x.OnRelease);
+            }
+            else { area.Visibility = Visibility.Collapsed; }
+            _isUpdatingUI = false;
+        }
+
+        private void OnPress_Changed(object sender, RoutedEventArgs e)
+        {
+            if (_isUpdatingUI) return;
+            var list = MainTab.SelectedItem == KeyTab ? KeyListBox : MouseListBox;
+            foreach (ISoundSetting item in list.SelectedItems) item.OnPress = (sender as CheckBox)?.IsChecked ?? false;
+        }
+        private void OnRelease_Changed(object sender, RoutedEventArgs e)
+        {
+            if (_isUpdatingUI) return;
+            var list = MainTab.SelectedItem == KeyTab ? KeyListBox : MouseListBox;
+            foreach (ISoundSetting item in list.SelectedItems) item.OnRelease = (sender as CheckBox)?.IsChecked ?? false;
+        }
 
         private void WaitButton_Click(object sender, RoutedEventArgs e)
         {
             if (_isWaitingInput) return;
             _isWaitingInput = true;
             WaitButton.Content = "入力待機中...";
-
             Window window = Window.GetWindow(this);
-            if (window != null)
-            {
-                window.PreviewKeyDown += OnWaitKeyDown;
-                window.PreviewMouseDown += OnWaitMouseDown;
-                FocusManager.SetFocusedElement(window, window);
-            }
+            if (window != null) { window.PreviewKeyDown += OnWaitKeyDown; window.PreviewMouseDown += OnWaitMouseDown; FocusManager.SetFocusedElement(window, window); }
         }
-
         private void OnWaitKeyDown(object sender, KeyEventArgs e)
         {
             e.Handled = true;
             if (e.Key != Key.Escape)
             {
-                SearchBox.Text = (e.Key == Key.System ? e.SystemKey : e.Key).ToString();
+                var key = e.Key == Key.ImeProcessed ? e.ImeProcessedKey : (e.Key == Key.System ? e.SystemKey : e.Key);
+                var mods = Keyboard.Modifiers;
+                var setting = _settings.KeySettings.FirstOrDefault(x => x.Key == key && x.Modifiers == mods);
+                SearchBox.Text = setting?.Name ?? key.ToString();
             }
             EndWaiting();
         }
-
-        private void OnWaitMouseDown(object sender, MouseButtonEventArgs e)
-        {
-            e.Handled = true;
-            SearchBox.Text = e.ChangedButton.ToString();
-            EndWaiting();
-        }
-
-        private void EndWaiting()
-        {
-            Window window = Window.GetWindow(this);
-            if (window != null)
-            {
-                window.PreviewKeyDown -= OnWaitKeyDown;
-                window.PreviewMouseDown -= OnWaitMouseDown;
-            }
-            WaitButton.Content = "キー入力で検索";
-            _isWaitingInput = false;
-        }
-
+        private void OnWaitMouseDown(object sender, MouseButtonEventArgs e) { e.Handled = true; SearchBox.Text = e.ChangedButton.ToString(); EndWaiting(); }
+        private void EndWaiting() { Window window = Window.GetWindow(this); if (window != null) { window.PreviewKeyDown -= OnWaitKeyDown; window.PreviewMouseDown -= OnWaitMouseDown; } WaitButton.Content = "キー入力で検索"; _isWaitingInput = false; }
         private void Reset_Click(object sender, RoutedEventArgs e) => _settings.ResetAllSettings(true);
-        private void ApplyFolderToAll_Click(object sender, RoutedEventArgs e)
+
+        private void MainTab_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            var dialog = new OpenFileDialog { CheckFileExists = false, FileName = "フォルダ選択" };
+            if (e.OriginalSource != MainTab) return;
+            if (KeyListBox != null) KeyListBox.UnselectAll();
+            if (MouseListBox != null) MouseListBox.UnselectAll();
+        }
+
+        private void OtherButton_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as Button;
+            if (btn?.ContextMenu != null) { btn.ContextMenu.PlacementTarget = btn; btn.ContextMenu.IsOpen = true; }
+        }
+
+        private void PlaySample_Click(object sender, RoutedEventArgs e) { if ((sender as Button)?.Tag is string path) _settings.PlayDirect(path); }
+
+        private void AddPath_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog { Filter = "音声・動画|*.wav;*.mp3;*.mp4;*.avi;*.wmv|全ファイル|*.*", Multiselect = true };
+            if (dialog.ShowDialog() == true)
+            {
+                var list = MainTab.SelectedItem == KeyTab ? KeyListBox : MouseListBox;
+                foreach (ISoundSetting setting in list.SelectedItems) { foreach (var file in dialog.FileNames) if (!setting.Paths.Contains(file)) setting.Paths.Add(file); }
+            }
+        }
+        private void AddFolder_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog { CheckFileExists = false, ValidateNames = false, FileName = "フォルダ選択" };
             if (dialog.ShowDialog() == true)
             {
                 string path = Path.GetDirectoryName(dialog.FileName) ?? string.Empty;
                 if (string.IsNullOrEmpty(path)) return;
-                foreach (var s in _settings.KeySettings) if (!s.Paths.Contains(path)) s.Paths.Add(path);
-                foreach (var s in _settings.MouseSettings) if (!s.Paths.Contains(path)) s.Paths.Add(path);
+                var list = MainTab.SelectedItem == KeyTab ? KeyListBox : MouseListBox;
+                foreach (ISoundSetting setting in list.SelectedItems) if (!setting.Paths.Contains(path)) setting.Paths.Add(path);
             }
-        }
-        private void AddPath_Click(object sender, RoutedEventArgs e)
-        {
-            var dialog = new OpenFileDialog { Filter = "音声・動画|*.wav;*.mp3;*.mp4;*.avi;*.wmv|全ファイル|*.*", CheckFileExists = false };
-            if (dialog.ShowDialog() == true) ((sender as Button)?.DataContext as ISoundSetting)?.Paths.Add(dialog.FileName);
         }
         private void RemovePath_Click(object sender, RoutedEventArgs e)
         {
-            if ((sender as Button)?.DataContext is string path)
+            var list = MainTab.SelectedItem == KeyTab ? KeyListBox : MouseListBox;
+            var pathList = MainTab.SelectedItem == KeyTab ? KeyPathList : MousePathList;
+            var targets = pathList.SelectedItems.Cast<string>().ToList();
+            if (targets.Count == 0 && (sender as FrameworkElement)?.DataContext is string contextPath) targets.Add(contextPath);
+            if (targets.Count == 0) return;
+            foreach (ISoundSetting setting in list.SelectedItems) { foreach (var path in targets) setting.Paths.Remove(path); }
+        }
+        private void ClearPaths_Click(object sender, RoutedEventArgs e)
+        {
+            var list = MainTab.SelectedItem == KeyTab ? KeyListBox : MouseListBox;
+            foreach (ISoundSetting setting in list.SelectedItems) setting.Paths.Clear();
+        }
+        private void CopyPaths_Click(object sender, RoutedEventArgs e)
+        {
+            var list = MainTab.SelectedItem == KeyTab ? KeyListBox : MouseListBox;
+            var pathList = MainTab.SelectedItem == KeyTab ? KeyPathList : MousePathList;
+            if (pathList.SelectedItems.Count > 0) _settings.CopyBuffer = pathList.SelectedItems.Cast<string>().ToList();
+            else if (list.SelectedItems.Count > 0) _settings.CopyBuffer = (list.SelectedItems[0] as ISoundSetting)?.Paths.ToList();
+        }
+        private void PastePaths_Click(object sender, RoutedEventArgs e)
+        {
+            if (_settings.CopyBuffer == null) return;
+            var list = MainTab.SelectedItem == KeyTab ? KeyListBox : MouseListBox;
+            foreach (ISoundSetting setting in list.SelectedItems) { foreach (var path in _settings.CopyBuffer) if (!setting.Paths.Contains(path)) setting.Paths.Add(path); }
+        }
+
+        private void PathList_DragOver(object sender, DragEventArgs e) { e.Effects = e.Data.GetDataPresent(DataFormats.FileDrop) ? DragDropEffects.Copy : DragDropEffects.None; e.Handled = true; }
+        private void PathList_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
             {
-                foreach (var s in _settings.KeySettings) if (s.Paths.Contains(path)) { s.Paths.Remove(path); return; }
-                foreach (var s in _settings.MouseSettings) if (s.Paths.Contains(path)) { s.Paths.Remove(path); return; }
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                var list = MainTab.SelectedItem == KeyTab ? KeyListBox : MouseListBox;
+                if (list.SelectedItems.Count == 0) return;
+                foreach (ISoundSetting setting in list.SelectedItems) { foreach (var file in files) if (!setting.Paths.Contains(file)) setting.Paths.Add(file); }
             }
         }
+
+        private void ExportSetting_Click(object sender, RoutedEventArgs e)
+        {
+            var list = MainTab.SelectedItem == KeyTab ? KeyListBox : MouseListBox;
+            if (list.SelectedItems.Count == 0) return;
+            var dialog = new SaveFileDialog { Filter = "JSONファイル|*.json" };
+            if (dialog.ShowDialog() == true)
+            {
+                var exportList = list.SelectedItems.Cast<ISoundSetting>().Select(x => new { x.Name, x.OnPress, x.OnRelease, x.Paths }).ToList();
+                File.WriteAllText(dialog.FileName, JsonSerializer.Serialize(exportList));
+            }
+        }
+        private void ImportSetting_Click(object sender, RoutedEventArgs e)
+        {
+            var list = MainTab.SelectedItem == KeyTab ? KeyListBox : MouseListBox;
+            if (list.SelectedItems.Count == 0) return;
+            var dialog = new OpenFileDialog { Filter = "JSONファイル|*.json" };
+            if (dialog.ShowDialog() == true)
+            {
+                var json = File.ReadAllText(dialog.FileName);
+                try
+                {
+                    var dataList = JsonSerializer.Deserialize<List<JsonData>>(json);
+                    if (dataList == null) return;
+                    foreach (ISoundSetting setting in list.SelectedItems)
+                    {
+                        var data = dataList.FirstOrDefault(d => d.Name == setting.Name) ?? dataList[0];
+                        setting.OnPress = data.OnPress; setting.OnRelease = data.OnRelease;
+                        setting.Paths.Clear(); foreach (var p in data.Paths) setting.Paths.Add(p);
+                    }
+                }
+                catch { }
+            }
+        }
+        private class JsonData { public string Name { get; set; } = ""; public bool OnPress { get; set; } public bool OnRelease { get; set; } public List<string> Paths { get; set; } = new(); }
     }
 }
